a2cb57b44438931bbeda5c3e94a1c38430dd15b9
Shivani99raskar <86920070+Shivani99raskar@users.noreply.github.com>
2024-06-23 20:55:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find minimum and maximum element in an array/find-minimum-and-maximum-element-in-an-array.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            Pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+ class Pair  
+{  
+    long first, second;  
+    public Pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} 
+
+Java users need to return result in Pair class
+For Example -> return new Pair(minimum,maximum)
+*/
+
+class Solution 
+{
+    static Pair getMinMax(long a[], long n)  
+    {
+       long max=a[0];
+       for(int j=1;j<n;j++)
+       {
+           if(a[j]>max)
+           max=a[j];
+       }
+       long min=a[0];
+        for(int j=1;j<n;j++)
+       {
+           if(a[j]<min)
+           min=a[j];
+       }
+       return new Pair(min,max);
+    }
+}
