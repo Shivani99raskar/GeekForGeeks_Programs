a40c93bfe72bb1f81801b32f05ec6c53fd79efec
Shivani99raskar <86920070+Shivani99raskar@users.noreply.github.com>
2024-08-14 14:04:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.java
+++ Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.java
@@ -37,20 +37,16 @@ public class Main {
 
 class Solution {
     void immediateSmaller(int arr[], int n) {
-        
        
-        for(int i=0;i<n-1;i++)
-        {
-            if(arr[i]>arr[i+1])
-            {
-                arr[i]=arr[i+1];
+       for (int i = 0; i < n - 1; i++) { 
+            if (arr[i + 1] < arr[i]) { 
+                arr[i] = arr[i + 1];   
+            } else {
+                arr[i] = -1;           
             }
-            else
-            {
-                arr[i]=-1;
-            }
-          
         }
-        arr[n-1]=-1;
+        arr[n - 1] = -1; 
     }
+    
+    
 }
