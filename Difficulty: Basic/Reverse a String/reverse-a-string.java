341caad8a95478d6f743041d59a470ff7ed2f604
Shivani99raskar <86920070+Shivani99raskar@users.noreply.github.com>
2024-08-25 10:27:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,41 +1,28 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial template for Java
 
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class Driver
-{
-    public static void main(String args[])throws IOException
-    {
+public class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- >0)
-        {
-            String str = read.readLine();
-            System.out.println(new Reverse().reverseWord(str));
+        while (t-- > 0) {
+            String s = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.revStr(s));
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-
-class Reverse
-{
-    public static String reverseWord(String str)
-    {
-        String temp="";char ch;
-        for(int i=0;i<str.length();i++)
-        {
-            ch= str.charAt(i); 
-            temp= ch+temp;
-           
-        }
-        return temp;
+class Solution {
+    static String revStr(String s) {
+       StringBuffer stringBuffer=new StringBuffer(s);
+       stringBuffer.reverse();
+       return stringBuffer.toString();
     }
 }
