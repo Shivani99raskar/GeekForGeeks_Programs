da50461416d73ae9b24439781bae502176fda771
Shivani Raskar <86920070+Shivani99raskar@users.noreply.github.com>
2025-04-22 16:20:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,42 +1,37 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNum(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-
-    int missingNumber(int n, int arr[]) {
-        
-    int N=n;
-    int totalSum=N*(N+1)/2;
-    
-    int arraySum=0;
-        for(int i=0;i<n-1;i++)
-        {
-           arraySum=arraySum+arr[i];
+   int missingNum(int arr[]) {
+        // code here
+        int j=1;
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=j){
+                return j;
+            }
+            j++;
         }
-        return totalSum-arraySum;
+        return j;
+        
     }
 }
